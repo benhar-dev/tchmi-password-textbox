488c5a5b27ee1a7e9689b2d7d2b4b0075b699d18
Benjamin <10912845+ben-har@users.noreply.github.com>
2020-05-18 17:05:10 +1000

    initial commit

---
+++ DemoPassword/Scripts/SetTypePassword.js
@@ -0,0 +1,23 @@
+﻿// Keep this lines for a best effort IntelliSense of Visual Studio 2017.
+/// <reference path="C:\TwinCAT\Functions\TE2000-HMI-Engineering\Infrastructure\TcHmiFramework\Latest\Lib\jquery.d.ts" />
+/// <reference path="C:\TwinCAT\Functions\TE2000-HMI-Engineering\Infrastructure\TcHmiFramework\Latest\TcHmi.d.ts" />
+/// <reference path="C:\TwinCAT\Functions\TE2000-HMI-Engineering\Infrastructure\TcHmiFramework\Latest\Controls\System\TcHmiControl\Source.d.ts" />
+
+// Keep this lines for a best effort IntelliSense of Visual Studio 2013/2015.
+/// <reference path="C:\TwinCAT\Functions\TE2000-HMI-Engineering\Infrastructure\TcHmiFramework\Latest\Lib\jquery\jquery.js" />
+/// <reference path="C:\TwinCAT\Functions\TE2000-HMI-Engineering\Infrastructure\TcHmiFramework\Latest\TcHmi.js" />
+
+(function (TcHmi) {
+
+    var SetTypePassword = function (Textbox) {
+
+        var id = Textbox.getId();
+        var texboxElement = document.getElementById(id);
+        var texboxInnerInputField = texboxElement.getElementsByTagName("input")[0];
+
+        texboxInnerInputField.type = "Password";
+
+    };
+    
+    TcHmi.Functions.registerFunction('SetTypePassword', SetTypePassword);
+})(TcHmi);
